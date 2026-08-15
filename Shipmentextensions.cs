dd714505de71cab14048b1b40c02ac5ee45de17a
Safaa-khalil <safaa.khalil@msa.edu.eg>
2026-08-16 02:18:33 +0300

    ShipmentExtensions

--- Shipmentextensions.cs
+++ Shipmentextensions.cs
@@ -9,15 +9,15 @@ namespace G_NET_84_OOP05
     public static class ShipmentExtensions
     {
         #region ShipmentExtensions
-        public static string GetSummary(this Shipment shipment)
-        {
-            return $"{shipment.TrackingCode} | {shipment.ShipmentType} | {shipment.Weight} KG | {shipment.TrackingStatus}";
-        }
+        //public static string GetSummary(this Shipment shipment)
+        //{
+        //    return $"{shipment.TrackingCode} | {shipment.ShipmentType} | {shipment.Weight} KG | {shipment.TrackingStatus}";
+        //}
 
-        public static bool IsDelivered(this Shipment shipment)
-        {
-            return shipment.TrackingStatus == "Delivered";
-        }
+        //public static bool IsDelivered(this Shipment shipment)
+        //{
+        //    return shipment.TrackingStatus == "Delivered";
+        //}
         #endregion
     }
 }
